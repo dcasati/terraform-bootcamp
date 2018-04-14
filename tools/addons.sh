d1585d2791397fc742f681df8c866bc14a82c87d
Diego Casati <diego.casati@gmail.com>
2018-04-14 16:05:33 -0700

    add back cowsay

--- tools/addons.sh
+++ tools/addons.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 apt-get update \
-	    && DEBIAN_FRONTEND=noninteractive apt-get install -y lynx
+    && DEBIAN_FRONTEND=noninteractive apt-get install -y lynx cowsay
 
 cowsay "Howdy Azure Bootcamp!" > /tmp/cowsay.txt
