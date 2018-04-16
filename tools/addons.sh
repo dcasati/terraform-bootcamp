abeb4812054590bb85f6991a5b388c7808ec7865
Diego Casati <diego.casati@gmail.com>
2018-04-16 15:27:12 -0700

    shoot! missing the -y flag

--- tools/addons.sh
+++ tools/addons.sh
@@ -3,4 +3,4 @@
 apt-get update \
     && DEBIAN_FRONTEND=noninteractive apt-get install -y lynx
 
-apt-get install cowsay && cowsay "Howdy Azure Bootcamp!" > /tmp/cowsay.txt
+apt-get install -y cowsay && cowsay "Howdy Azure Bootcamp!" > /tmp/cowsay.txt
