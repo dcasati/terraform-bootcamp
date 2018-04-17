50efcae752e26197d545479ad99b4cfef4b21a27
Diego Casati <diego.casati@gmail.com>
2018-04-17 11:17:57 -0700

    removing the excution of cowsay

--- tools/addons.sh
+++ tools/addons.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 apt-get update \
-    && DEBIAN_FRONTEND=noninteractive apt-get install -y lynx cowsay \
-    && cowsay "Howdy Azure Bootcamp!" > /tmp/cowsay.txt
+    && DEBIAN_FRONTEND=noninteractive apt-get install -y lynx cowsay
