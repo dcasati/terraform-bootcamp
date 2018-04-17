456562e1e0b9c6959bc4185af76b6a7319eb79ec
Diego Casati <diego.casati@gmail.com>
2018-04-17 00:32:12 -0700

    .

--- tools/addons.sh
+++ tools/addons.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 apt-get update \
-    && DEBIAN_FRONTEND=noninteractive apt-get install -y lynx
-
-apt-get install -y cowsay && cowsay "Howdy Azure Bootcamp!" > /tmp/cowsay.txt
+    && DEBIAN_FRONTEND=noninteractive apt-get install -y lynx cowsay \
+    && cowsay "Howdy Azure Bootcamp!" > /tmp/cowsay.txt
